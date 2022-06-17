6f4ceaa36d3fbac331c3769362e77ea9cf8c217a
lucasbarreto92 <lucasbs.code@gmail.com>
2022-06-16 23:43:43 -0300

    Creation of components Header and Input

---
+++ React-Version/jokenpo/src/components/Input.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Input extends React.Component {
+  render() {
+    return(
+      <div>
+        <input type="text" />
+        <button>Play!</button>
+      </div>
+    );
+  }
+}
+
+export default Input;
